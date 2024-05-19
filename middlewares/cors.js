06d86a41f33d6c437a94037c243b01b81dc9feae
serfly3 <153284546+serfly3@users.noreply.github.com>
2024-05-19 22:06:15 +0500

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,7 +1,7 @@
 const allowedCors = [
   "https://practicum.yandex.ru",
   "https://students-projects.ru",
-  "https://pindie-serfly.nomoredomainswork.ru",
+  "https://pindie-serfly.nomoredomainswork.ru/api",
   "localhost:3000",
   "localhost:3001"
 ];
